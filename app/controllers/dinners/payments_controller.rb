5b38804669bea61b105c2e7e7e692893306d7fcc
Miguel Torres <migueltg93@gmail.com>
2018-02-24 14:16:09 -0600

    Atomic Transaction w/Stripe

--- app/controllers/dinners/payments_controller.rb
+++ app/controllers/dinners/payments_controller.rb
@@ -1,4 +1,7 @@
 class Dinners::PaymentsController < ApplicationController
+      rescue_from ActiveRecord::RecordInvalid do |exception|
+        redirect_to new_dinner_payment_path, alert: "Vous êtes déjà membre de ce dîner"
+      end
 
   def index 
   end
@@ -12,28 +15,41 @@ class Dinners::PaymentsController < ApplicationController
   end
 
   def create
-    @dinner = Dinner.find(params[:dinner_id])
-    token = params[:stripeToken]
+    begin
+      @dinner = Dinner.find(params[:dinner_id])
+      token = params[:stripeToken]
+  
+        @guest = Guest.new(
+          :is_guest => true,
+          :dinner => @dinner,
+          :user => current_user
+        )
+  
+      ActiveRecord::Base.transaction do 
+    
+        @guest.save!
+    
+         # Charge the user's card:
+        charge = Stripe::Charge.create(
+        :amount => (@dinner.cost * 100),
+        :currency => "eur",
+        :description => "Example charge",
+        :source => token,
+        :capture => false,
+        )
+  
+      end
+  
+      if @guest.save 
+        @dinner.update(:guest => @dinner.guest - 1)
+        charge.capture
+        redirect_to @dinner, notice: "Bon Appetit! Votre reservation a été fait!"
+      else
+        redirect_to new_dinner_payment_path, alert: "Vous êtes déjà membre de ce dîner Deuxime"
+      end
 
-    # Charge the user's card:
-    charge = Stripe::Charge.create(
-    :amount => (@dinner.cost * 100),
-    :currency => "eur",
-    :description => "Example charge",
-    :source => token,
-    )
-
-    new_guest = Guest.new(
-      :is_guest => true,
-      :dinner => @dinner,
-      :user => current_user
-    )
-
-    new_guest.save
-
-    @dinner.guest = @dinner.guest - 1
-
-
+      
+    end
   end
 
   def destroy 
