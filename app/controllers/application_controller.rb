e92e13b0a2b73cbda8fd321c3be6373acd8c76d0
Miguel Torres <migueltg93@gmail.com>
2018-03-01 19:45:53 -0600

    Shows email if there's no name attribute

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,9 +11,9 @@ class ApplicationController < ActionController::Base
 
 protected  
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:username, :email, :password,
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :username, :email, :password,
       :password_confirmation, :remember_me, :avatar, :avatar_cache, :remove_avatar) }
-    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:username, :email, :password,
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :username, :email, :password,
       :password_confirmation, :current_password, :avatar, :avatar_cache, :remove_avatar) }
   end
 
