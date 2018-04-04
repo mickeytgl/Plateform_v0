6f0f25ee272b66d17f107ee05e655d6d03325343
Miguel Torres <migueltg93@gmail.com>
2018-04-04 15:42:37 -0500

    Added bias to current location on Google Places API

--- app/javascript/packs/search/index.js
+++ app/javascript/packs/search/index.js
@@ -2,4 +2,23 @@ document.addEventListener("turbolinks:load", function() {
   input = document.getElementById('autocomplete');
   options = { types: ['(cities)'] };
   autocomplete = new google.maps.places.Autocomplete(input, options);
-})
+
+  // Bias the autocomplete object to the user's geographical location,
+  // as supplied by the browser's 'navigator.geolocation' object.
+  function geolocate() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        var geolocation = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
+        var circle = new google.maps.Circle({
+          center: geolocation,
+          radius: position.coords.accuracy
+        });
+        autocomplete.setBounds(circle.getBounds());
+      });
+    }
+  }
+});
+
