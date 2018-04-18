c5c8d5fb0c8f1efcb7362bf8144d9f4be632fd44
Miguel Torres <migueltg93@gmail.com>
2018-04-18 15:06:30 -0500

    clean up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,31 +4,9 @@ class UsersController < ApplicationController
   def show
   end
 
-  def new_payment_source
-  end
-
-  def payment_source
-    token = params[:stripeToken]
-
-    customer = Stripe::Customer.create({
-      email: current_user.email,
-      source: token,
-    })
-
-    payment = PaymentSource.create(
-      user:           current_user,
-      card_last4:     params[:card_last4],
-      card_exp_month: params[:card_exp_month],
-      card_exp_year:  params[:card_exp_year],
-      card_brand:     params[:card_brand]
-    )
-
-    redirect_to root_path, notice: "Hurray, your debit card has been saved"
-  end
-
   private 
 
-    def set_user 
+    def set_user
     	@user = User.find(params[:id])
     end
 end
